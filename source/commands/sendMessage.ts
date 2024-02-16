4f6e530f1faae45562d5b8d0210be4627b0fe105
Georgi Stoyanov <stoyanow2707@gmail.com>
2024-02-16 17:12:18 +0200

    feat(sendMessage.ts): added new command

---
+++ source/commands/sendMessage.ts
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder } = require('discord.js');
+import { CommandInteraction, CommandInteractionOptionResolver, Options } from 'discord.js';
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('sendmessage')
+        .setDescription('Send a message to a channel!')
+        .addStringOption((option: any) =>
+            option.setName('message')
+                .setDescription('The message to send')
+                .setRequired(true)),
+    async execute(interaction: CommandInteraction) {
+        const options = interaction.options as CommandInteractionOptionResolver;
+        const message = options.getString('message'), channel = interaction.channel;
+        if (channel) {
+            await channel.send(message as string);
+            await interaction.deferReply({ ephemeral: true });
+            await interaction.deleteReply();
+        } else {
+            await interaction.reply({ content: 'Could not determine the channel to send the message.', ephemeral: true });
+        }
+    },
+};
