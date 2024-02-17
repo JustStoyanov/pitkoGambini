4f6a2e9e75cc0cfbea1d747a88a23b16ec3d9d67
Georgi Stoyanov <stoyanow2707@gmail.com>
2024-02-17 13:50:06 +0200

    chore(sendMessage): added a way to create a new line

--- source/commands/sendMessage.ts
+++ source/commands/sendMessage.ts
@@ -19,7 +19,9 @@ module.exports = {
         }
 
         try {
-            await interaction.channel.send(options.getString('message', true));
+            const message = options.getString('message', true);
+            const formattedMessage = message.replace(/\\n/g, '\n');
+            await interaction.channel.send(formattedMessage);
             await interaction.reply({ content: 'Message sent successfully!', ephemeral: true });
         } catch (error) {
             console.error('Error sending message:', error);
