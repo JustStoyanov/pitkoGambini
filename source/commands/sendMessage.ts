1464606aa3a01e00ee8b4a921102e8ec12da0093
Georgi Stoyanov <stoyanow2707@gmail.com>
2024-02-17 13:14:49 +0200

    chore(sendMessage): modifications and bug fixes

--- source/commands/sendMessage.ts
+++ source/commands/sendMessage.ts
@@ -1,23 +1,29 @@
-const { SlashCommandBuilder } = require('discord.js');
-import { CommandInteraction, CommandInteractionOptionResolver } from 'discord.js';
+import { SlashCommandBuilder, CommandInteraction, CommandInteractionOptionResolver, TextChannel } from 'discord.js';
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('send_message')
         .setDescription('Send a message to a channel!')
-        .addStringOption((option: any) =>
+        .addStringOption(option =>
             option.setName('message')
                 .setDescription('The message to send')
                 .setRequired(true)),
     async execute(interaction: CommandInteraction) {
+        if (!interaction.isCommand() || !interaction.guild) {
+            return;
+        }
         const options = interaction.options as CommandInteractionOptionResolver;
-        const message = options.getString('message'), channel = interaction.channel;
-        if (channel) {
-            await channel.send(message as string);
-            await interaction.deferReply({ ephemeral: true });
-            await interaction.deleteReply();
-        } else {
-            await interaction.reply({ content: 'Could not determine the channel to send the message.', ephemeral: true });
+        if (!(interaction.channel instanceof TextChannel)) {
+            await interaction.reply({ content: 'This command can only be used in a guild text channel.', ephemeral: true });
+            return;
+        }
+
+        try {
+            await interaction.channel.send(options.getString('message', true));
+            await interaction.reply({ content: 'Message sent successfully!', ephemeral: true });
+        } catch (error) {
+            console.error('Error sending message:', error);
+            await interaction.reply({ content: 'There was an error while sending the message.', ephemeral: true });
         }
     },
 };
