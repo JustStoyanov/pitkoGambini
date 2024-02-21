d80ed52517278342f8708ac0d620a9c7ac9a758a
Georgi Stoyanov <stoyanow2707@gmail.com>
2024-02-21 10:02:46 +0200

    feat(onStart): added random statuses

--- source/modules/onStart.ts
+++ source/modules/onStart.ts
@@ -1,9 +1,15 @@
 import { Client, ActivityType } from 'discord.js';
+const config = require('../../config.json');
 
 module.exports = (client: Client) => {
     client.once('ready', () => {
         client.user?.setStatus('dnd');
         client.user?.setUsername('Pitko Gambini');
-        client.user?.setActivity('Competing on Grove Street', { type: ActivityType.Custom });
+        while (true) {
+            setTimeout(() => {
+                const status = config.statuses[Math.floor(Math.random() * config.statuses.length)];
+                client.user?.setActivity(status, { type: ActivityType.Custom });
+            }, 1000 * 60 * 60);
+        };
     });
 };
